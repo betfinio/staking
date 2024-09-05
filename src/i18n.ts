0f4318f479fa4034e216db9fae5190d0c92a4e0f
bfmonster <bf.monster@proton.me>
2024-09-05 22:13:14 +0200

    added cz

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,9 +2,12 @@ import * as i18 from 'i18next';
 import type { i18n } from 'i18next';
 import ICU from 'i18next-icu';
 import { initReactI18next } from 'react-i18next';
+import czJSON from './translations/cz.json';
 import enJSON from './translations/en.json';
 import ruJSON from './translations/ru.json';
 
+// @ts-ignore
+import czShared from 'betfinio_app/locales/cz';
 // @ts-ignore
 import enShared from 'betfinio_app/locales/en';
 // @ts-ignore
@@ -21,6 +24,12 @@ const resources = {
 		translation: {
 			staking: ruJSON,
 			shared: ruShared,
+		},
+	},
+	cz: {
+		translation: {
+			staking: czJSON,
+			shared: czShared,
 		},
 	},
 };
