c3f0d260f982679e171e52aece1d869c204e292d
bfmonster <bf.monster@proton.me>
2024-09-16 10:46:59 +0200

    added loggers

--- src/lib/gql/conservative/index.ts
+++ src/lib/gql/conservative/index.ts
@@ -1,9 +1,12 @@
 import { GetStakedDocument, type GetStakedQuery, execute } from '@/.graphclient';
+import logger from '@/src/config/logger.ts';
 import type { ExecutionResult } from 'graphql/execution';
 import type { Address } from 'viem';
 
 export const requestConservativeStakes = async (staker: Address) => {
+	logger.start('[conservative]', 'fetching stakes', staker);
 	const data: ExecutionResult<GetStakedQuery> = await execute(GetStakedDocument, { staker, staking: import.meta.env.PUBLIC_CONSERVATIVE_STAKING_ADDRESS });
+	logger.success('[conservative]', 'fetching stakes', data.data?.stakeds.length);
 	return data.data?.stakeds.map((stake) => {
 		return {
 			amount: BigInt(stake.amount),
--- src/lib/gql/dynamic/index.ts
+++ src/lib/gql/dynamic/index.ts
@@ -1,9 +1,12 @@
 import { GetStakedDocument, type GetStakedQuery, execute } from '@/.graphclient';
+import logger from '@/src/config/logger';
 import type { ExecutionResult } from 'graphql/execution';
 import type { Address } from 'viem';
 
 export const requestDynamicStakes = async (staker: Address) => {
+	logger.start('[dynamic]', 'fetching stakes', staker);
 	const data: ExecutionResult<GetStakedQuery> = await execute(GetStakedDocument, { staker, staking: import.meta.env.PUBLIC_DYNAMIC_STAKING_ADDRESS });
+	logger.success('[dynamic]', 'fetching stakes', data.data?.stakeds.length);
 	return data.data?.stakeds.map((stake) => {
 		return {
 			amount: BigInt(stake.amount),
