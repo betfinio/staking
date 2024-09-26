ca00af30d0d726c7613985ceb710082bd1f34d82
Monster <bf.monster@proton.me>
2024-09-26 12:11:31 +0200

    added to rsbuild hsared dep

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -89,6 +89,10 @@ export default defineConfig({
 							singleton: true,
 							requiredVersion: dependencies.wagmi,
 						},
+						'i18next-browser-languagedetector': {
+							singleton: true,
+							requiredVersion: dependencies['i18next-browser-languagedetector'],
+						},
 					},
 				}),
 			],
