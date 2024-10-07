ef35f38cc16e38869e70c5d38dac14a96ef345b2
Monster <bf.monster@proton.me>
2024-10-07 10:26:15 +0200

    combined dyn stakes to one

--- src/lib/gql/dynamic/index.ts
+++ src/lib/gql/dynamic/index.ts
@@ -1,5 +1,6 @@
 import { GetStakedDocument, type GetStakedQuery, execute } from '@/.graphclient';
 import logger from '@/src/config/logger';
+import type { Stake } from 'betfinio_app/lib/types';
 import type { ExecutionResult } from 'graphql/execution';
 import type { Address } from 'viem';
 
@@ -7,16 +8,19 @@ export const requestDynamicStakes = async (staker: Address) => {
 	logger.start('[dynamic]', 'fetching stakes', staker);
 	const data: ExecutionResult<GetStakedQuery> = await execute(GetStakedDocument, { staker, staking: import.meta.env.PUBLIC_DYNAMIC_STAKING_ADDRESS });
 	logger.success('[dynamic]', 'fetching stakes', data.data?.stakeds.length);
-	return data.data?.stakeds.map((stake) => {
-		return {
-			amount: BigInt(stake.amount),
-			start: Number(stake.blockTimestamp),
-			pool: stake.pool as Address,
-			reward: BigInt(stake.reward),
-			staker: stake.staker as Address,
-			hash: stake.transactionHash as Address,
-			end: Number(stake.unlock),
-			ended: false,
-		};
-	});
+	if (data.data) {
+		return data.data.stakeds.map((stake) => {
+			return {
+				amount: BigInt(stake.amount),
+				start: Number(stake.blockTimestamp),
+				pool: stake.pool as Address,
+				reward: BigInt(stake.reward),
+				staker: stake.staker as Address,
+				hash: stake.transactionHash as Address,
+				end: Number(stake.unlock),
+				ended: false,
+			} as Stake;
+		});
+	}
+	return [];
 };
