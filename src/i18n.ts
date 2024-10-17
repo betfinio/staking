b617a3da8e6dc8117fefde291b61ebdbe0c2294e
Plane <bf.plane@proton.me>
2024-10-17 19:59:28 +0200

    add navigator language detection

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,6 @@
 import { sharedLang } from 'betfinio_app/locales/index';
 import type { i18n } from 'i18next';
 import * as i18 from 'i18next';
-import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
 import ICU from 'i18next-icu';
 import { initReactI18next } from 'react-i18next';
 import czStaking from './translations/cz/staking.json';
@@ -9,6 +8,7 @@ import enStaking from './translations/en/staking.json';
 import ruStaking from './translations/ru/staking.json';
 
 export const defaultNS = 'staking';
+export const defaultLocale = 'en';
 
 export const resources = {
 	en: {
@@ -23,12 +23,15 @@ export const resources = {
 		staking: czStaking,
 		shared: sharedLang.cz,
 	},
+	cs: {
+		games: czStaking,
+		shared: sharedLang.cz,
+	},
 } as const;
 
 const instance: i18n = i18.createInstance();
 instance
 	.use(initReactI18next)
-	.use(I18nextBrowserLanguageDetector)
 	.use(ICU)
 	.init({
 		resources,
@@ -37,5 +40,19 @@ instance
 		interpolation: { escapeValue: false },
 		react: { useSuspense: true },
 	});
+
+const changeLanguage = async (locale: string | null) => {
+	const lng = locale ?? defaultLocale;
+	await instance.changeLanguage(lng);
+	localStorage.setItem('i18nextLng', lng);
+};
+
+if (!localStorage.getItem('i18nextLng')) {
+	const locale = navigator.language.split('-')[0];
+	console.log(locale, changeLanguage);
+	changeLanguage(locale);
+} else {
+	changeLanguage(localStorage.getItem('i18nextLng'));
+}
 
 export default instance;
